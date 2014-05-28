1370bafba5c26fb63cf1c1412a9df6d452b452ef
Jan-Philip Loos <maxdaten@gmail.com>
2014-05-28 04:50:44 +0200

    fix normal for front and back face

--- src/Yage/Geometry/D3/Cube.hs
+++ src/Yage/Geometry/D3/Cube.hs
@@ -15,38 +15,40 @@ import Yage.Geometry.Elements
 cube :: (Fractional a) => V3 a -> Primitive (Vertex (P3T2NT3 pn tx nn tg a))
 cube dim = 
   let V3 x y z  = dim / 2.0
-      right     = Face  (pos =: (V3 x   y    z ) <+> tex =: (V2 0 0) <+> norm =: (V3 1 0 0) <+> tang =: (V3 0 0 (-1)))
-                        (pos =: (V3 x (-y)   z ) <+> tex =: (V2 1 0) <+> norm =: (V3 1 0 0) <+> tang =: (V3 0 0 (-1)))
-                        (pos =: (V3 x (-y) (-z)) <+> tex =: (V2 1 1) <+> norm =: (V3 1 0 0) <+> tang =: (V3 0 0 (-1)))
-                        (pos =: (V3 x   y  (-z)) <+> tex =: (V2 0 1) <+> norm =: (V3 1 0 0) <+> tang =: (V3 0 0 (-1)))
+                        -- position               | uv       | normal        | tangent
+      right     = Face  ( vertex (V3 x   y    z )    (V2 0 0)  (V3 1 0 0)      (V3 0 0 (-1)) )
+                        ( vertex (V3 x (-y)   z )    (V2 1 0)  (V3 1 0 0)      (V3 0 0 (-1)) )
+                        ( vertex (V3 x (-y) (-z))    (V2 1 1)  (V3 1 0 0)      (V3 0 0 (-1)) )
+                        ( vertex (V3 x   y  (-z))    (V2 0 1)  (V3 1 0 0)      (V3 0 0 (-1)) )
 
-      left      = Face  (pos =: (V3 (-x)   y  (-z)) <+> tex =: (V2 0 0) <+> norm =: (V3 (-1) 0 0) <+> tang =: (V3 0 0 1))
-                        (pos =: (V3 (-x) (-y) (-z)) <+> tex =: (V2 1 0) <+> norm =: (V3 (-1) 0 0) <+> tang =: (V3 0 0 1))
-                        (pos =: (V3 (-x) (-y)   z ) <+> tex =: (V2 1 1) <+> norm =: (V3 (-1) 0 0) <+> tang =: (V3 0 0 1))
-                        (pos =: (V3 (-x)   y    z ) <+> tex =: (V2 0 1) <+> norm =: (V3 (-1) 0 0) <+> tang =: (V3 0 0 1))
+      left      = Face  ( vertex (V3 (-x)   y  (-z)) (V2 0 0)  (V3 (-1) 0 0)   (V3 0 0 1) )
+                        ( vertex (V3 (-x) (-y) (-z)) (V2 1 0)  (V3 (-1) 0 0)   (V3 0 0 1) )
+                        ( vertex (V3 (-x) (-y)   z ) (V2 1 1)  (V3 (-1) 0 0)   (V3 0 0 1) )
+                        ( vertex (V3 (-x)   y    z ) (V2 0 1)  (V3 (-1) 0 0)   (V3 0 0 1) )
       
-      top       = Face  (pos =: (V3 (-x) y (-z)) <+> tex =: (V2 0 0) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
-                        (pos =: (V3 (-x) y   z ) <+> tex =: (V2 1 0) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
-                        (pos =: (V3   x  y   z ) <+> tex =: (V2 1 1) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
-                        (pos =: (V3   x  y (-z)) <+> tex =: (V2 0 1) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
+      top       = Face  ( vertex (V3 (-x) y (-z))    (V2 0 0)  (V3 0 1 0)      (V3 1 0 0) )
+                        ( vertex (V3 (-x) y   z )    (V2 1 0)  (V3 0 1 0)      (V3 1 0 0) )
+                        ( vertex (V3   x  y   z )    (V2 1 1)  (V3 0 1 0)      (V3 1 0 0) )
+                        ( vertex (V3   x  y (-z))    (V2 0 1)  (V3 0 1 0)      (V3 1 0 0) )
 
-      bottom    = Face  (pos =: (V3   x  (-y) (-z)) <+> tex =: (V2 0 0) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 1))
-                        (pos =: (V3   x  (-y)   z ) <+> tex =: (V2 1 0) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 1))
-                        (pos =: (V3 (-x) (-y)   z ) <+> tex =: (V2 1 1) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 1))
-                        (pos =: (V3 (-x) (-y) (-z)) <+> tex =: (V2 0 1) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 1))
+      bottom    = Face  ( vertex (V3   x  (-y) (-z)) (V2 0 0)  (V3 0 (-1) 0)   (V3 (-1) 0 1) )
+                        ( vertex (V3   x  (-y)   z ) (V2 1 0)  (V3 0 (-1) 0)   (V3 (-1) 0 1) )
+                        ( vertex (V3 (-x) (-y)   z ) (V2 1 1)  (V3 0 (-1) 0)   (V3 (-1) 0 1) )
+                        ( vertex (V3 (-x) (-y) (-z)) (V2 0 1)  (V3 0 (-1) 0)   (V3 (-1) 0 1) )
       
-      front     = Face  (pos =: (V3 (-x)   y  z) <+> tex =: (V2 0 0) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
-                        (pos =: (V3 (-x) (-y) z) <+> tex =: (V2 1 0) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
-                        (pos =: (V3   x  (-y) z) <+> tex =: (V2 1 1) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
-                        (pos =: (V3   x    y  z) <+> tex =: (V2 0 1) <+> norm =: (V3 0 1 0) <+> tang =: (V3 1 0 0))
+      front     = Face  ( vertex (V3 (-x)   y  z)    (V2 0 0)  (V3 0 0 1)      (V3 1 0 0) )
+                        ( vertex (V3 (-x) (-y) z)    (V2 1 0)  (V3 0 0 1)      (V3 1 0 0) )
+                        ( vertex (V3   x  (-y) z)    (V2 1 1)  (V3 0 0 1)      (V3 1 0 0) )
+                        ( vertex (V3   x    y  z)    (V2 0 1)  (V3 0 0 1)      (V3 1 0 0) )
 
-      back      = Face  (pos =: (V3   x    y  (-z)) <+> tex =: (V2 0 0) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 0))
-                        (pos =: (V3   x  (-y) (-z)) <+> tex =: (V2 1 0) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 0))
-                        (pos =: (V3 (-x) (-y) (-z)) <+> tex =: (V2 1 1) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 0))
-                        (pos =: (V3 (-x)   y  (-z)) <+> tex =: (V2 0 1) <+> norm =: (V3 0 (-1) 0) <+> tang =: (V3 (-1) 0 0))
+      back      = Face  ( vertex (V3   x    y  (-z)) (V2 0 0)  (V3 0 0 (-1))   (V3 (-1) 0 0) )
+                        ( vertex (V3   x  (-y) (-z)) (V2 1 0)  (V3 0 0 (-1))   (V3 (-1) 0 0) )
+                        ( vertex (V3 (-x) (-y) (-z)) (V2 1 1)  (V3 0 0 (-1))   (V3 (-1) 0 0) )
+                        ( vertex (V3 (-x)   y  (-z)) (V2 0 1)  (V3 0 0 (-1))   (V3 (-1) 0 0) )
   in Cube right left top bottom front back
   where
-    pos = position3
-    tex = texture2
-    norm = normal3
-    tang = tangent3
+    vertex :: V3 a -> V2 a -> V3 a -> V3 a -> Vertex (P3T2NT3 pn tx nn tg a)
+    vertex p t n b = position3 =: p <+>
+                     texture2  =: t <+>
+                     normal3   =: n <+>
+                     tangent3  =: b
